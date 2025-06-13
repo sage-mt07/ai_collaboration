b0269c8e35aa105f5f43f9370b324e3c0b980402
seijim <seiji7m@gmail.com>
2025-06-13 19:28:23 +0900

    up

--- oss/src/SchemaRegistry/ISchemaRegistryClient.cs
+++ oss/src/SchemaRegistry/ISchemaRegistryClient.cs
@@ -5,12 +5,13 @@ using System.Threading.Tasks;
 namespace KsqlDsl.SchemaRegistry;
 
 /// <summary>
-/// Schema registry client interface for registering and managing Kafka schemas (Avro only)
+/// Schema registry client interface for registering and managing Kafka Avro schemas ONLY
+/// KsqlDsl supports Avro format exclusively for schema management
 /// </summary>
 public interface ISchemaRegistryClient : IDisposable
 {
     /// <summary>
-    /// Registers both key and value schemas for the specified topic
+    /// Registers both key and value Avro schemas for the specified topic
     /// </summary>
     /// <param name="topicName">The topic name</param>
     /// <param name="keySchema">The key schema (Avro format)</param>
@@ -19,7 +20,7 @@ public interface ISchemaRegistryClient : IDisposable
     Task<(int keySchemaId, int valueSchemaId)> RegisterTopicSchemasAsync(string topicName, string keySchema, string valueSchema);
 
     /// <summary>
-    /// Registers a key schema for the specified topic
+    /// Registers an Avro key schema for the specified topic
     /// </summary>
     /// <param name="topicName">The topic name</param>
     /// <param name="keySchema">The key schema (Avro format)</param>
@@ -27,7 +28,7 @@ public interface ISchemaRegistryClient : IDisposable
     Task<int> RegisterKeySchemaAsync(string topicName, string keySchema);
 
     /// <summary>
-    /// Registers a value schema for the specified topic
+    /// Registers an Avro value schema for the specified topic
     /// </summary>
     /// <param name="topicName">The topic name</param>
     /// <param name="valueSchema">The value schema (Avro format)</param>
@@ -35,51 +36,49 @@ public interface ISchemaRegistryClient : IDisposable
     Task<int> RegisterValueSchemaAsync(string topicName, string valueSchema);
 
     /// <summary>
-    /// Registers a new schema for the specified subject (legacy method)
+    /// Registers a new Avro schema for the specified subject (legacy method)
     /// </summary>
     /// <param name="subject">The subject name (typically topic-value or topic-key)</param>
-    /// <param name="schema">The schema to register</param>
+    /// <param name="avroSchema">The Avro schema to register</param>
     /// <returns>The schema ID assigned by the registry</returns>
-    Task<int> RegisterSchemaAsync(string subject, string schema);
+    Task<int> RegisterSchemaAsync(string subject, string avroSchema);
 
     /// <summary>
-    /// Gets the latest schema for the specified subject
+    /// Gets the latest Avro schema for the specified subject
     /// </summary>
     /// <param name="subject">The subject name</param>
-    /// <returns>The latest schema information</returns>
-    Task<SchemaInfo> GetLatestSchemaAsync(string subject);
+    /// <returns>The latest Avro schema information</returns>
+    Task<AvroSchemaInfo> GetLatestSchemaAsync(string subject);
 
     /// <summary>
-    /// Gets a specific schema by ID
+    /// Gets a specific Avro schema by ID
     /// </summary>
     /// <param name="schemaId">The schema ID</param>
-    /// <returns>The schema information</returns>
-    Task<SchemaInfo> GetSchemaByIdAsync(int schemaId);
+    /// <returns>The Avro schema information</returns>
+    Task<AvroSchemaInfo> GetSchemaByIdAsync(int schemaId);
 
     /// <summary>
-    /// Checks if a schema is compatible with the latest version of the subject
+    /// Checks if an Avro schema is compatible with the latest version of the subject
     /// </summary>
     /// <param name="subject">The subject name</param>
-    /// <param name="schema">The schema to check</param>
+    /// <param name="avroSchema">The Avro schema to check</param>
     /// <returns>True if compatible, false otherwise</returns>
-    Task<bool> CheckCompatibilityAsync(string subject, string schema);
+    Task<bool> CheckCompatibilityAsync(string subject, string avroSchema);
 
     /// <summary>
-    /// Gets all versions of a schema for the specified subject
+    /// Gets all versions of an Avro schema for the specified subject
     /// </summary>
     /// <param name="subject">The subject name</param>
     /// <returns>List of version numbers</returns>
     Task<IList<int>> GetSchemaVersionsAsync(string subject);
 
     /// <summary>
-    /// Gets a specific version of a schema for the specified subject
+    /// Gets a specific version of an Avro schema for the specified subject
     /// </summary>
     /// <param name="subject">The subject name</param>
     /// <param name="version">The version number</param>
-    /// <returns>The schema information</returns>
-    Task<SchemaInfo> GetSchemaAsync(string subject, int version);
-
-
+    /// <returns>The Avro schema information</returns>
+    Task<AvroSchemaInfo> GetSchemaAsync(string subject, int version);
 
     /// <summary>
     /// Gets all subjects registered in the schema registry
@@ -88,3 +87,18 @@ public interface ISchemaRegistryClient : IDisposable
     Task<IList<string>> GetAllSubjectsAsync();
 }
 
+/// <summary>
+/// Avro schema information container (KsqlDsl specific)
+/// </summary>
+public class AvroSchemaInfo
+{
+    public int Id { get; set; }
+    public int Version { get; set; }
+    public string Subject { get; set; } = string.Empty;
+    public string AvroSchema { get; set; } = string.Empty;
+    
+    /// <summary>
+    /// Always Avro in KsqlDsl context
+    /// </summary>
+    public string SchemaFormat => "AVRO";
+}
